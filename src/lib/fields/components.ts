c0176c4446dd8f49b90d2e12adc111eaaac2aaa7
bienoubien-studio <salut@bienoubien.studio>
2024-12-04 15:21:36 +0100

    Fix - bad imports ...

--- src/lib/fields/components.ts
+++ src/lib/fields/components.ts
@@ -1,21 +1,21 @@
-import Blocks from './fields/blocks/component/Blocks.svelte';
-import Checkbox from './fields/checkbox/component/Checkbox.svelte';
-import ComboBox from './fields/combobox/component/ComboBox.svelte';
-// import Component from './fields/component/Component.svelte';
-import Date from './fields/date/component/Date.svelte';
-import Email from './fields/email/component/Email.svelte';
-// import Group from './fields/group/component/Group.svelte';
-import Link from './fields/link/component/Link.svelte';
-import Number from './fields/number/component/Number.svelte';
-import Radio from './fields/radio/component/Radio.svelte';
-import Relation from './fields/relation/component/Relation.svelte';
-import RichText from './fields/rich-text/component/RichText.svelte';
-import Select from './fields/select/component/Select.svelte';
-import Separator from './fields/separator/component/Separator.svelte';
-import Slug from './fields/slug/component/Slug.svelte';
-import Tabs from './fields/tabs/component/Tabs.svelte';
-import Text from './fields/text/component/Text.svelte';
-import Toggle from './fields/toggle/component/Toggle.svelte';
+import Blocks from './blocks/component/Blocks.svelte';
+import Checkbox from './checkbox/component/Checkbox.svelte';
+import ComboBox from './combobox/component/ComboBox.svelte';
+// import Component from './component/Component.svelte';
+import Date from './date/component/Date.svelte';
+import Email from './email/component/Email.svelte';
+// import Group from './group/component/Group.svelte';
+import Link from './link/component/Link.svelte';
+import Number from './number/component/Number.svelte';
+import Radio from './radio/component/Radio.svelte';
+import Relation from './relation/component/Relation.svelte';
+import RichText from './rich-text/component/RichText.svelte';
+import Select from './select/component/Select.svelte';
+import Separator from './separator/component/Separator.svelte';
+import Slug from './slug/component/Slug.svelte';
+import Tabs from './tabs/component/Tabs.svelte';
+import Text from './text/component/Text.svelte';
+import Toggle from './toggle/component/Toggle.svelte';
 export {
 	Blocks,
 	Checkbox,
